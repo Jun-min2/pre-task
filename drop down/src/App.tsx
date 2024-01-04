c74344d2ca5221cfa2392878b7663ed5cce2746d
Jun-min2 <cjm990622@naver.com>
2024-01-05 02:17:50 +0900

    test: dropdown

--- drop down/src/App.tsx
+++ drop down/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react';
+// import Dropdown from './Dropdown';
 
-function App() {
-  const [count, setCount] = useState(0)
+// const App = () => {
+//   const [view, setView] = useState(false);
 
+//   return (
+//     <div>
+//       <ul
+//         onClick={() => {
+//           setView(!view);
+//         }}
+//       >
+//         반가워요, nickname 님! {view ? '⌃' : '⌄'}
+//         {view && <Dropdown />}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import Dropdown from './Dropdown';
+import { useState } from 'react';
+const App = () => {
+  const [dropdownVisibility, setDropdownVisibility] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app">
+      <button onClick={() => setDropdownVisibility(!dropdownVisibility)}>
+        {dropdownVisibility ? 'Close' : 'Open'}
+      </button>
+      <Dropdown visibility={dropdownVisibility}>
+        <ul>
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+          <li>item 4</li>
+        </ul>
+      </Dropdown>
+    </div>
+  );
+};
 
-export default App
+export default App;
