af56928edde35dbd9ee13145d92ad583a5c9acb2
Jun-min2 <cjm990622@naver.com>
2024-01-05 03:16:41 +0900

    test: drop

--- drop down/src/App.tsx
+++ drop down/src/App.tsx
@@ -1,44 +1,7 @@
-// import { useState } from 'react';
-// import Dropdown from './Dropdown';
+import React from 'react';
 
-// const App = () => {
-//   const [view, setView] = useState(false);
-
-//   return (
-//     <div>
-//       <ul
-//         onClick={() => {
-//           setView(!view);
-//         }}
-//       >
-//         반가워요, nickname 님! {view ? '⌃' : '⌄'}
-//         {view && <Dropdown />}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-import Dropdown from './Dropdown';
-import { useState } from 'react';
 const App = () => {
-  const [dropdownVisibility, setDropdownVisibility] = useState(false);
-  return (
-    <div className="app">
-      <button onClick={() => setDropdownVisibility(!dropdownVisibility)}>
-        {dropdownVisibility ? 'Close' : 'Open'}
-      </button>
-      <Dropdown visibility={dropdownVisibility}>
-        <ul>
-          <li>item 1</li>
-          <li>item 2</li>
-          <li>item 3</li>
-          <li>item 4</li>
-        </ul>
-      </Dropdown>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
